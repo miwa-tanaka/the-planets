8d2cc528b611aa34887dff64714d09cbbb86514d
Miwa <miwa.tnk9675@gmail.com>
2022-07-31 11:32:48 +0900

    created 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,53 +1,29 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import Header from "../components/header"
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import "../scss/index.scss"
 
-// markup
+import earth from "../images/earthOverview.png"
+
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <div className="pageWrapper">
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+      <Header />
+      <main>
+        <div className="errorPageWrapper">
+          <h1>
+            4
+            <img src={earth} alt="0" loading="lazy" width="450" height="450" />
+            4
+          </h1>
+          <p>page not found</p>
+          <Link to="/" aria-label="Go to the top page">Home</Link>
+        </div>
+      </main>
+    </div>
   )
 }
 
