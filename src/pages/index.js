d5c4887164b6ace24572b834062df59bbe335288
Miwa <miwa.tnk9675@gmail.com>
2022-07-31 13:05:40 +0900

    added alt, width and height

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,49 +25,49 @@ const IndexPage = () => {
           <Link to="/mercury/" aria-label="Got to the mercury page" className="planetWrapper">
             <h1>Mercury</h1>
             <div className="planetImg">
-              <img src={mercury} />
+              <img src={mercury} alt="mercury" loading="lazy" width="290" height="290" />
             </div>
           </Link>
           <Link to="/venus/" aria-label="Got to the venus page" className="planetWrapper">
             <h1>Venus</h1>
             <div className="planetImg">
-              <img src={venus} />
+              <img src={venus} alt="venus" loading="lazy" width="400" height="400" />
             </div>
           </Link>
           <Link to="/earth/" aria-label="Got to the earth page"  className="planetWrapper">
             <h1>Earth</h1>
             <div className="planetImg">
-              <img src={earth} />
+              <img src={earth} alt="earth" loading="lazy" width="450" height="450" />
             </div>
           </Link>
           <Link to="/mars/" aria-label="Got to the mars page" className="planetWrapper">
             <h1>Mars</h1>
             <div className="planetImg">
-              <img src={mars} />
+              <img src={mars} alt="mars" loading="lazy" width="336" height="336" />
             </div>
           </Link>
           <Link to="/jupiter/" aria-label="Got to the jupiter page" className="planetWrapper">
             <h1>Jupiter</h1>
             <div className="planetImg">
-              <img src={jupiter} />
+              <img src={jupiter} alt="jupiter" loading="lazy" width="582" height="582" />
             </div>
           </Link>
           <Link to="/saturn/" aria-label="Got to the saturn page" className="planetWrapper">
             <h1>Saturn</h1>
             <div className="planetImg">
-              <img src={saturn} />
+              <img src={saturn} alt="saturn" loading="lazy" width="668" height="668" />
             </div>
           </Link>
           <Link to="/uranus/" aria-label="Got to the uranus page" className="planetWrapper">
             <h1>Uranus</h1>
             <div className="planetImg">
-              <img src={uranus} />
+              <img src={uranus} alt="uranus" loading="lazy" width="458" height="458" />
             </div>
           </Link>
           <Link to="/neptune/" aria-label="Got to the neptune page" className="planetWrapper">
             <h1>Neptune</h1>
             <div className="planetImg">
-              <img src={neptune} />
+              <img src={neptune} alt="neptune" loading="lazy" width="450" height="450" />
             </div>
           </Link>
         </div>
