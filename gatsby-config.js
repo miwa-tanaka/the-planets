d4291f9960a6411423621c6566d8d3cddefe38b1
Miwa <miwa.tnk9675@gmail.com>
2022-07-31 11:13:21 +0900

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: `/the-planets`,
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://miwa-tanaka.github.io/the-planets/`
   },
   plugins: [
     "gatsby-plugin-sass",
@@ -21,6 +21,18 @@ module.exports = {
       options: {
         path: `./src/data/`,
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ThePlanets`,
+        short_name: `ThePlanets`,
+        start_url: `/`,
+        background_color: `#070724`,
+        theme_color: `#6D2ED5`,
+        display: `standalone`,
+        icon: "src/images/favicon.png",
+      },
+    },
   ],
 };
